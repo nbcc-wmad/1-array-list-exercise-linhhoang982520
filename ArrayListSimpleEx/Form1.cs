e07bc8461db7a7d7ff569091ecdf58bd470d102f
linhhoang982520 <49657938+linhhoang982520@users.noreply.github.com>
2019-04-25 09:17:16 -0300

    Array exercise-Linh
    Array exercise

--- ArrayListSimpleEx/Form1.cs
+++ ArrayListSimpleEx/Form1.cs
@@ -7,14 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace ArrayListSimpleEx
 {
     public partial class Form1 : Form
     {
+        ArrayList messages = new ArrayList();
+        bool check = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            messages.Add("I");
+            messages.Add("Love");
+            messages.Add("Programming");
+            messages.Add("So");
+            messages.Add("Much");
+        }
+
+        private void btnShowMsg_Click(object sender, EventArgs e)
+        {
+            if (!check)
+            {
+                Messages();
+            }
+            else if (check)
+            {
+                check = false;
+                messages.Reverse();
+                Messages();
+            }
+        }
+
+        private void Messages()
+        {
+            string mess = string.Empty;
+
+            for (int i = 0; i < messages.Count; i++)
+            {
+                mess += messages[i].ToString() + " ";
+            }
+
+            lblMessage.Text = mess;
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            if (!check)
+            {
+                messages.Reverse();
+                check = true;
+                Messages();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!check)
+            {
+                messages.Insert(1, txtSecondPos.Text);
+                Messages();
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (!check)
+            {
+                messages.RemoveAt(1);
+                Messages();
+            }
         }
     }
 }
